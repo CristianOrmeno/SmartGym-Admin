ce2a0760f2d20513def73addff78da3c4b151a4b
Cristian Rodolfo Ormeño <cristianormeno@live.com.ar>
2026-03-24 22:08:53 -0300

    Create Actividad schema for activities

---
+++ backend/src/models/Actividad.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const actividadSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Actividad', actividadSchema);
